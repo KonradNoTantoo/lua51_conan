edb9d907f90542d7af4b45d592088d66443cc467
Knife <konrad@no.tantoo>
2020-02-02 22:29:04 +0100

    Useless filter

--- build.py
+++ build.py
@@ -4,10 +4,4 @@ from cpt.packager import ConanMultiPackager
 if __name__ == "__main__":
     builder = ConanMultiPackager()
     builder.add_common_builds(shared_option_name="lua:shared")
-    filtered_builds = []
-    for settings, options, env_vars, build_requires, reference in builder.items:
-        if settings["compiler"] == "Visual Studio" or options["shared"] == "False":
-             filtered_builds.append([settings, options, env_vars, build_requires, reference])
-    builder.builds = filtered_builds
-    builder.run()
     builder.run()
