90c71597b6e525aa9571c557532cd9c8a431ff8b
Knife <konrad@no.tantoo>
2020-02-02 20:00:51 +0100

    Add fPIC option

--- conanfile.py
+++ conanfile.py
@@ -13,8 +13,8 @@ class LuaConan(ConanFile):
     description = "Lua is a powerful, efficient, lightweight, embeddable scripting language."
     topics = ("language", "scripting", "embedded")
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "build_interpreter": [True, False], "build_compiler": [True, False]}
-    default_options = "shared=True", "build_interpreter=False", "build_compiler=False"
+    options = {"shared": [True, False], "fPIC": [True, False], "build_interpreter": [True, False], "build_compiler": [True, False]}
+    default_options = "shared=True", "fPIC=True", "build_interpreter=False", "build_compiler=False"
     exports_sources = "CMakeLists.txt", "windows/*"
     generators = "cmake"
 
@@ -27,8 +27,11 @@ class LuaConan(ConanFile):
         if self.settings.os != "Windows" and self.options.build_interpreter:
             self.requires("readline/7.0@bincrafters/stable", private=True)
 
-    def config(self):
+    def configure(self):
         del self.settings.compiler.libcxx
+        del self.settings.compiler.cppstd
+        if self.settings.compiler == 'Visual Studio':
+            del self.options.fPIC
 
     def source(self):
         zip_name = "lua-%s.tar.gz" % self.version
@@ -36,6 +39,10 @@ class LuaConan(ConanFile):
         tools.get(tarball_path)
         if self.settings.compiler == "Visual Studio":
             self.copy_file_to_source("CMakeLists.txt")
+        elif bool(self.options.fPIC):
+            # Makefiles ignore anything passed on make command line
+            tools.replace_in_file("{}/src/Makefile".format(self.folder_name), "CFLAGS= ", "CFLAGS= -fPIC ")
+
 
     def build(self):
         if self.settings.compiler == "Visual Studio":
@@ -52,6 +59,7 @@ class LuaConan(ConanFile):
             with tools.chdir(self.folder_name):
                 env_build = AutoToolsBuildEnvironment(self)
                 env_build.make(target=str(self.settings.os).lower())
+
 
     def package(self):
         src_path = os.path.join(self.folder_name, "src")
